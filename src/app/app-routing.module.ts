5a4b157495ece099a5a5f1fd95b4058d6216a167
mashaapt <melancholia.888@gmail.com>
2022-03-08 22:16:41 +0100

    started nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+
+  {
+    path: 'products',
+    loadChildren: () => import('./pages/products/products.module').then(m => m.ProductsModule)
+  },
+
+  {
+    path: 'payment',
+    loadChildren: () => import('./pages/payment/payment.module').then(m => m.PaymentModule)
+  },
+
+  {
+    path: 'delivery',
+    loadChildren: () => import('./pages/delivery/delivery.module').then(m => m.DeliveryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
