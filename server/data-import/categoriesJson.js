c68a220382b15820de9173b947718d3a03a9a6dd
mashaapt <melancholia.888@gmail.com>
2022-03-25 16:43:43 +0100

    IMports

---
+++ server/data-import/categoriesJson.js
@@ -0,0 +1,8 @@
+db.categories.insert([
+    // {
+    //     title: 'War and Peace',
+    //     genre: 'Historical Fiction',
+    //     author: 'Lev Nikolayevich Tolstoy',
+    //     read: false
+    // }
+])
